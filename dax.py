4c7878aa8f392ba1c930e6d65e65222c615c385b
Jose Perdomo <joseperdomo2428@gmail.com>
2024-06-21 18:11:22 -0400

    12

--- dax.py
+++ dax.py
@@ -3,8 +3,14 @@ VAR currentId = archivo[ResultRecords.ResultRecord.Id.#text]
 RETURN
 CALCULATE(
     FIRSTNONBLANK(
-        archivo[ResultRecords.ResultRecord.AuditRecords.AuditRecord.Note],
-        archivo[ResultRecords.ResultRecord.AuditRecords.AuditRecord.Action] = "NewNote" &&
-        archivo[ResultRecords.ResultRecord.Id.#text] = currentId
+        SELECTCOLUMNS(
+            FILTER(
+                archivo, 
+                archivo[ResultRecords.ResultRecord.AuditRecords.AuditRecord.Action] = "NewNote" &&
+                archivo[ResultRecords.ResultRecord.Id.#text] = currentId
+            ),
+            "Note", archivo[ResultRecords.ResultRecord.AuditRecords.AuditRecord.Note]
+        ), 
+        archivo[ResultRecords.ResultRecord.AuditRecords.AuditRecord.Note]
     )
 )
