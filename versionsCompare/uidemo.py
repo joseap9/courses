aece8f92d057640d4ced3490ed69275679a38125
Jose Perdomo <joseperdomo2428@gmail.com>
2024-07-15 16:00:32 -0400

    vc14

--- versionsCompare/uidemo.py
+++ versionsCompare/uidemo.py
@@ -1,6 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QVBoxLayout, QWidget, QHBoxLayout, QLabel, QScrollArea
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFileDialog, QVBoxLayout, QWidget, QHBoxLayout, QLabel, QScrollArea, QSplitter
+from PyQt5.QtCore import Qt, QEvent
 from PyQt5.QtGui import QPixmap
 import fitz  # PyMuPDF
 import tempfile
@@ -22,7 +22,8 @@ class PDFComparer(QMainWindow):
         self.button2.clicked.connect(self.load_second_pdf)
         self.layout.addWidget(self.button2)
 
-        self.pdf_layout = QHBoxLayout()
+        self.splitter = QSplitter(Qt.Horizontal)
+        self.layout.addWidget(self.splitter)
 
         self.pdf1_scroll = QScrollArea(self)
         self.pdf1_container = QWidget()
@@ -30,7 +31,6 @@ class PDFComparer(QMainWindow):
         self.pdf1_container.setLayout(self.pdf1_layout)
         self.pdf1_scroll.setWidget(self.pdf1_container)
         self.pdf1_scroll.setWidgetResizable(True)
-        self.pdf_layout.addWidget(self.pdf1_scroll)
 
         self.pdf2_scroll = QScrollArea(self)
         self.pdf2_container = QWidget()
@@ -38,9 +38,9 @@ class PDFComparer(QMainWindow):
         self.pdf2_container.setLayout(self.pdf2_layout)
         self.pdf2_scroll.setWidget(self.pdf2_container)
         self.pdf2_scroll.setWidgetResizable(True)
-        self.pdf_layout.addWidget(self.pdf2_scroll)
 
-        self.layout.addLayout(self.pdf_layout)
+        self.splitter.addWidget(self.pdf1_scroll)
+        self.splitter.addWidget(self.pdf2_scroll)
 
         container = QWidget()
         container.setLayout(self.layout)
@@ -51,12 +51,20 @@ class PDFComparer(QMainWindow):
         self.pdf1_path = None
         self.pdf2_path = None
 
+        self.pdf1_scroll.verticalScrollBar().valueChanged.connect(self.sync_scroll)
+        self.pdf2_scroll.verticalScrollBar().valueChanged.connect(self.sync_scroll)
+
+    def sync_scroll(self, value):
+        if self.sender() == self.pdf1_scroll.verticalScrollBar():
+            self.pdf2_scroll.verticalScrollBar().setValue(value)
+        elif self.sender() == self.pdf2_scroll.verticalScrollBar():
+            self.pdf1_scroll.verticalScrollBar().setValue(value)
+
     def load_first_pdf(self):
         options = QFileDialog.Options()
         fileName, _ = QFileDialog.getOpenFileName(self, "Select First PDF", "", "PDF Files (*.pdf);;All Files (*)", options=options)
         if fileName:
             self.button1.setText(fileName.split('/')[-1])
-            self.pdf1_text, self.pdf1_words = self.extract_text_and_positions(fileName)
             self.pdf1_path = fileName
             self.compare_pdfs()
 
@@ -65,25 +73,27 @@ class PDFComparer(QMainWindow):
         fileName, _ = QFileDialog.getOpenFileName(self, "Select Second PDF", "", "PDF Files (*.pdf);;All Files (*)", options=options)
         if fileName:
             self.button2.setText(fileName.split('/')[-1])
-            self.pdf2_text, self.pdf2_words = self.extract_text_and_positions(fileName)
             self.pdf2_path = fileName
             self.compare_pdfs()
 
     def extract_text_and_positions(self, file_path):
         document = fitz.open(file_path)
-        text = ""
+        text = []
         words = []
 
         for page_num in range(document.page_count):
             page = document.load_page(page_num)
-            text += page.get_text()
+            text.append(page.get_text())
             word_list = page.get_text("words")
-            words.extend(word_list)
+            words.append(word_list)
 
         return text, words
 
     def compare_pdfs(self):
-        if self.pdf1_text and self.pdf2_text:
+        if self.pdf1_path and self.pdf2_path:
+            self.pdf1_text, self.pdf1_words = self.extract_text_and_positions(self.pdf1_path)
+            self.pdf2_text, self.pdf2_words = self.extract_text_and_positions(self.pdf2_path)
+
             temp_pdf1_path = self.highlight_differences(self.pdf1_path, self.pdf1_words, self.pdf2_words)
             temp_pdf2_path = self.highlight_differences(self.pdf2_path, self.pdf2_words, self.pdf1_words)
 
@@ -92,11 +102,13 @@ class PDFComparer(QMainWindow):
 
     def highlight_differences(self, file_path, words1, words2):
         doc = fitz.open(file_path)
-        words2_set = set((word[4] for word in words2))  # Set of words in the second document
 
         for page_num in range(len(doc)):
             page = doc.load_page(page_num)
-            for word in words1:
+            words1_set = set((word[4] for word in words1[page_num]))
+            words2_set = set((word[4] for word in words2[page_num]))
+
+            for word in words1[page_num]:
                 if word[4] not in words2_set:
                     highlight = fitz.Rect(word[:4])
                     page.add_highlight_annot(highlight)
@@ -107,6 +119,9 @@ class PDFComparer(QMainWindow):
         return temp_pdf_path
 
     def display_pdfs(self, layout, file_path):
+        for i in reversed(range(layout.count())):
+            layout.itemAt(i).widget().deleteLater()
+
         doc = fitz.open(file_path)
         for page_num in range(len(doc)):
             page = doc.load_page(page_num)
