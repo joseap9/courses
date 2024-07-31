41111dcb5eaa252dcc16de6f122c458650d7d4fd
Jose Perdomo <joseperdomo2428@gmail.com>
2024-07-31 16:21:19 -0400

    st cxfreeeze

--- versionsCompare/setup.py
+++ versionsCompare/setup.py
@@ -1,21 +1,21 @@
-from distutils.core import setup
-import py2exe
 import sys
-import os
+from cx_Freeze import setup, Executable
 
-sys.argv.append('py2exe')
+# Incluir las librerías necesarias
+build_exe_options = {
+    "packages": ["os", "sys", "PyQt5", "fitz", "tempfile"],
+    "includes": ["PyQt5.QtWidgets", "PyQt5.QtCore", "PyQt5.QtGui"],
+    "excludes": ["tkinter"],  # Excluir tkinter si no se usa
+}
 
-script_path = os.path.join('src', 'uidemo.py')
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"  # Esto asegura que no se abra una consola de comandos
 
 setup(
-    options={
-        'py2exe': {
-            'includes': ['PyQt5.QtWidgets', 'PyQt5.QtCore', 'PyQt5.QtGui', 'fitz', 'tempfile'],
-            'dll_excludes': ['MSVCP90.dll'],  # Excluir MSVC DLLs si es necesario
-            'excludes': ['tkinter'],  # Excluir tkinter si no se usa
-        }
-    },
-    windows=[{
-        'script': script_path,
-    }],
+    name="PDFComparer",
+    version="1.0",
+    description="A simple PDF comparison tool",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("src/uidemo.py", base=base)],
 )
