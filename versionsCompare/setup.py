8d47d3b55e4c8b61ff8eed949e097c43969e70e2
Jose Perdomo <joseperdomo2428@gmail.com>
2024-07-31 17:12:20 -0400

    qq

--- versionsCompare/setup.py
+++ versionsCompare/setup.py
@@ -1,21 +1,19 @@
-import sys
 from cx_Freeze import setup, Executable
+import sys
 
-# Incluir las librerías necesarias
-build_exe_options = {
-    "packages": ["os", "sys", "PyQt5", "fitz", "tempfile", "pymupdf"],
-    "includes": ["PyQt5.QtWidgets", "PyQt5.QtCore", "PyQt5.QtGui", "mupdf"],
-    "excludes": ["tkinter"],  # Excluir tkinter si no se usa
-}
+# Incluir archivos adicionales (si tienes archivos como imágenes, etc.)
+files = ['app.log']
 
-base = None
-if sys.platform == "win32":
-    base = "Win32GUI"  # Esto asegura que no se abra una consola de comandos
-
+# Construcción del ejecutable
 setup(
-    name="PDFComparer",
-    version="1.0",
-    description="A simple PDF comparison tool",
-    options={"build_exe": build_exe_options},
-    executables=[Executable("src/uidemo.py", base=base)],
+    name='PDFComparer',
+    version='1.0',
+    description='PDF Comparison Tool',
+    options={
+        'build_exe': {
+            'include_files': files,
+            'packages': ['os', 'sys', 'PyQt5', 'fitz', 'tempfile']
+        }
+    },
+    executables=[Executable('tu_script.py', base='Win32GUI' if sys.platform == 'win32' else None)]
 )
