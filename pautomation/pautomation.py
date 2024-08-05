f88cb68354456d0adf0d61f45c955e91a5e70b22
Jose Perdomo <joseperdomo2428@gmail.com>
2024-08-05 12:53:39 -0400

    cc

--- pautomation/pautomation.py
+++ pautomation/pautomation.py
@@ -1,3 +1,5 @@
 indexOf(outputs('Concat Body Text'), 'Assessment Status:')
 
 trim(first(split(substring(outputs('Concat Body Text'), add(outputs('Find Assessment Status'), length('Assessment Status:')), 100), ' ')))
+
+if(contains(outputs('Extract Assessment Status'), 'New'), 'New', if(contains(outputs('Extract Assessment Status'), 'Pending Information/Remediation'), 'Pending Information/Remediation', if(contains(outputs('Extract Assessment Status'), 'Complete'), 'Complete', 'Unknown')))
