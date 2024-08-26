6b8faeabb2a4f99e0addebe451f170ccf17919e3
Jose Perdomo <joseperdomo2428@gmail.com>
2024-08-26 15:24:14 -0400

    asfasf

--- versionsCompare/uidemov5.py
+++ versionsCompare/uidemov5.py
@@ -1,19 +1,17 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget, QHBoxLayout, QFrame, QMessageBox
-from PyQt5.QtCore import Qt, QUrl
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QFileDialog, QHBoxLayout
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QUrl
 import fitz  # PyMuPDF
 
 class PDFComparer(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Configuración de la ventana principal
         self.setWindowTitle("PDF Comparer")
-        self.setGeometry(100, 100, 1200, 800)
+        self.setGeometry(100, 100, 1600, 900)
 
-        # Layout principal
-        self.main_layout = QVBoxLayout()
+        layout = QVBoxLayout()
 
         # Sección de botones para cargar PDFs
         self.button_layout = QHBoxLayout()
@@ -26,22 +24,20 @@ class PDFComparer(QMainWindow):
         self.load_pdf2_button.clicked.connect(self.load_second_pdf)
         self.button_layout.addWidget(self.load_pdf2_button)
 
-        self.main_layout.addLayout(self.button_layout)
+        layout.addLayout(self.button_layout)
 
-        # Sección del visualizador de PDF
-        self.pdf_view = QWebEngineView(self)
-        self.main_layout.addWidget(self.pdf_view)
+        # Sección de visualización de PDFs
+        self.viewer_layout = QHBoxLayout()
 
-        # Sección de navegación entre páginas y diferencias
+        self.pdf1_viewer = QWebEngineView(self)
+        self.pdf2_viewer = QWebEngineView(self)
+        self.viewer_layout.addWidget(self.pdf1_viewer)
+        self.viewer_layout.addWidget(self.pdf2_viewer)
+
+        layout.addLayout(self.viewer_layout)
+
+        # Sección de navegación entre diferencias
         self.navigation_layout = QHBoxLayout()
-
-        self.prev_page_button = QPushButton("Previous Page", self)
-        self.prev_page_button.clicked.connect(self.prev_page)
-        self.navigation_layout.addWidget(self.prev_page_button)
-
-        self.next_page_button = QPushButton("Next Page", self)
-        self.next_page_button.clicked.connect(self.next_page)
-        self.navigation_layout.addWidget(self.next_page_button)
 
         self.prev_diff_button = QPushButton("Previous Difference", self)
         self.prev_diff_button.clicked.connect(self.prev_difference)
@@ -51,10 +47,10 @@ class PDFComparer(QMainWindow):
         self.next_diff_button.clicked.connect(self.next_difference)
         self.navigation_layout.addWidget(self.next_diff_button)
 
-        self.main_layout.addLayout(self.navigation_layout)
+        layout.addLayout(self.navigation_layout)
 
         container = QWidget()
-        container.setLayout(self.main_layout)
+        container.setLayout(layout)
         self.setCentralWidget(container)
 
         # Inicialización de variables
@@ -68,15 +64,13 @@ class PDFComparer(QMainWindow):
     def load_first_pdf(self):
         self.pdf1_path, _ = QFileDialog.getOpenFileName(self, "Select First PDF", "", "PDF Files (*.pdf)")
         if self.pdf1_path:
-            self.current_page = 0
-            self.pdf_view.setUrl(QUrl.fromLocalFile(self.pdf1_path))
+            self.pdf1_viewer.setUrl(QUrl.fromLocalFile(self.pdf1_path))
             self.extract_differences()
 
     def load_second_pdf(self):
         self.pdf2_path, _ = QFileDialog.getOpenFileName(self, "Select Second PDF", "", "PDF Files (*.pdf)")
         if self.pdf2_path:
-            self.current_page = 0
-            self.pdf_view.setUrl(QUrl.fromLocalFile(self.pdf2_path))
+            self.pdf2_viewer.setUrl(QUrl.fromLocalFile(self.pdf2_path))
             self.extract_differences()
 
     def extract_differences(self):
@@ -88,10 +82,14 @@ class PDFComparer(QMainWindow):
             # Lógica simplificada para extraer diferencias
             self.differences = []
             for i in range(self.total_pages):
-                text1 = doc1[i].get_text()
-                text2 = doc2[i].get_text()
-                if text1 != text2:
-                    self.differences.append(i)
+                text1 = doc1[i].get_text("blocks")
+                text2 = doc2[i].get_text("blocks")
+                differences_in_page = []
+                for block1, block2 in zip(text1, text2):
+                    if block1[4] != block2[4]:
+                        differences_in_page.append((block1, block2))
+                if differences_in_page:
+                    self.differences.append((i, differences_in_page))
 
             if self.differences:
                 self.current_difference_index = 0
@@ -99,26 +97,32 @@ class PDFComparer(QMainWindow):
 
     def go_to_difference(self):
         if self.differences:
-            diff_page = self.differences[self.current_difference_index]
+            diff_page, differences_in_page = self.differences[self.current_difference_index]
             self.current_page = diff_page
-            self.pdf_view.setUrl(QUrl.fromLocalFile(self.pdf1_path if self.current_difference_index % 2 == 0 else self.pdf2_path))
+
+            # Resalta las diferencias en ambas páginas
+            self.highlight_page_differences(self.pdf1_path, differences_in_page, self.pdf1_viewer, 0)
+            self.highlight_page_differences(self.pdf2_path, differences_in_page, self.pdf2_viewer, 1)
+
             self.update_navigation_buttons()
+
+    def highlight_page_differences(self, pdf_path, differences_in_page, viewer, pdf_index):
+        doc = fitz.open(pdf_path)
+        page = doc[self.current_page]
+
+        for block in differences_in_page:
+            block_rect = fitz.Rect(block[pdf_index][:4])
+            highlight = page.add_highlight_annot(block_rect)
+            highlight.set_colors({"stroke": (1, 0, 0)})  # Rojo
+            highlight.update()
+
+        temp_path = f"temp_{pdf_index}_page_{self.current_page}.pdf"
+        doc.save(temp_path)
+        viewer.setUrl(QUrl.fromLocalFile(temp_path))
 
     def update_navigation_buttons(self):
         self.prev_diff_button.setEnabled(self.current_difference_index > 0)
         self.next_diff_button.setEnabled(self.current_difference_index < len(self.differences) - 1)
-        self.prev_page_button.setEnabled(self.current_page > 0)
-        self.next_page_button.setEnabled(self.current_page < self.total_pages - 1)
-
-    def next_page(self):
-        if self.current_page < self.total_pages - 1:
-            self.current_page += 1
-            self.pdf_view.setUrl(QUrl.fromLocalFile(self.pdf1_path if self.current_page % 2 == 0 else self.pdf2_path))
-
-    def prev_page(self):
-        if self.current_page > 0:
-            self.current_page -= 1
-            self.pdf_view.setUrl(QUrl.fromLocalFile(self.pdf1_path if self.current_page % 2 == 0 else self.pdf2_path))
 
     def next_difference(self):
         if self.current_difference_index < len(self.differences) - 1:
