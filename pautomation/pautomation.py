f0f29430d0eff27db6d0727632f9d1036e3299cf
Jose Perdomo <joseperdomo2428@gmail.com>
2024-09-06 11:40:34 -0400

    qw

--- pautomation/pautomation.py
+++ pautomation/pautomation.py
@@ -7,3 +7,33 @@ if(contains(outputs('Extract Assessment Status'), 'New'), 'New', if(contains(out
 addDays(triggerOutputs()?['body/receiveDateTime'], if(equals(coalesce(outputs('Date_Code_SLA'), 0), 20), 20, if(equals(coalesce(outputs('Date_Code_SLA'), 0), 15), 15, 5)))
 
 addDays(triggerOutputs()?['body/receiveDateTime'], if(equals(outputs('Date_Code_SLA'), '20'), 20, if(equals(outputs('Date_Code_SLA'), '15'), 15, 5)))
+
+if(
+    or(
+        equals(outputs('IOGloblalAssesmentStatus_Value'), 'New'),
+        equals(outputs('ChileApprovalStatus_Value'), 'New')
+    ), 
+    'New', 
+    if(
+        or(
+            equals(outputs('IOGloblalAssesmentStatus_Value'), 'Pending IO DPO Review'),
+            equals(outputs('ChileApprovalStatus_Value'), 'Pending IO DPO Review')
+        ), 
+        'Pending IO DPO Review', 
+        if(
+            or(
+                equals(outputs('IOGloblalAssesmentStatus_Value'), 'IO Review Complete'),
+                equals(outputs('ChileApprovalStatus_Value'), 'IO Review Complete')
+            ), 
+            'IO Review Complete', 
+            if(
+                or(
+                    equals(outputs('IOGloblalAssesmentStatus_Value'), 'Withdrawn or Unapproved'),
+                    equals(outputs('ChileApprovalStatus_Value'), 'Withdrawn or Unapproved')
+                ), 
+                'Withdrawn or Unapproved', 
+                'Unknown'
+            )
+        )
+    )
+)
